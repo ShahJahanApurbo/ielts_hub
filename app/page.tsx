c99b7a7a0c11694b41aa7bbd12487cabcec0e6eb
Md Shajahan Apurba <shahapurbo007@gmail.com>
2024-08-08 18:42:37 +0600

    Experimented the line chart from Tremor react libary

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,104 @@
-export default function Home() {
-  return <h1>Hello world</h1>;
+"use client";
+
+import { LineChart } from "@tremor/react";
+
+const chartdata = [
+  {
+    date: "Jan 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "Feb 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "Mar 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "Apr 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "May 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "Jun 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "Jul 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "Aug 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "Sep 22",
+    Noyon: 6.5,
+    Apurbo: 5.5,
+    Nurul: 7.5,
+    Nadira: 8.5,
+  },
+  {
+    date: "Oct 22",
+    Noyon: 6.5,
+    Apurbo: 7.5,
+    Nurul: 7.5,
+    Nadira: 7.5,
+  },
+  {
+    date: "Nov 22",
+    Noyon: 6.5,
+    Apurbo: 6.5,
+    Nurul: 9,
+    Nadira: 5.5,
+  },
+];
+
+const dataFormatter = (number: number) => `Band ${number}`;
+
+export default function LineChartHero() {
+  return (
+    <LineChart
+      className="h-80"
+      data={chartdata}
+      index="date"
+      categories={["Noyon", "Apurbo", "Nurul", "Nadira"]}
+      colors={["indigo", "rose", "lime", "amber"]}
+      valueFormatter={dataFormatter}
+      curveType="linear"
+      maxValue={9}
+      minValue={4}
+      yAxisWidth={60}
+      intervalType="preserveStartEnd"
+      onValueChange={(v) => console.log(v)}
+    />
+  );
 }
